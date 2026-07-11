c3d1d388336501c4183d3beba0c9c82d74e2bc82
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-11 11:57:58 +0530

    Refactor Test servlet to use ManagerFactory and forward user data to test.jsp

--- src/main/java/lk/kaushalya/bcd/jta/Test.java
+++ src/main/java/lk/kaushalya/bcd/jta/Test.java
@@ -1,8 +1,6 @@
 package lk.kaushalya.bcd.jta;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,14 +8,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JTA-PU");
-        EntityManager em = emf.createEntityManager();
 
-        System.out.println(em);
+        /// When a user sends a request, create a new EntityManagerFactory for that request.
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JTA-PU");
+//        System.out.println(emf.hashCode());
+//        EntityManager em = emf.createEntityManager();
+
+        EntityManager em = ManagerFactory.getEntityManager();
+
+        List<Object> result = em.createNativeQuery("SELECT * FROM user").getResultList();
+        req.setAttribute("yourDataAttribute", result);
+        req.getRequestDispatcher("/test.jsp").forward(req, resp);
     }
 }
