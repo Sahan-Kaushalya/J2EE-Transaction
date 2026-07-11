87c8b93188a40d1a4a5f93f4a785b832221af069
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-11 11:57:14 +0530

    Add ManagerFactory.java for JTA entity manager creation

---
+++ src/main/java/lk/kaushalya/bcd/jta/ManagerFactory.java
@@ -0,0 +1,23 @@
+package lk.kaushalya.bcd.jta;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class ManagerFactory {
+    public static EntityManagerFactory factory;
+
+    static {
+        factory = Persistence.createEntityManagerFactory("JTA-PU");
+    }
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
+    public static void close(){
+        if(factory.isOpen()){
+            factory.close();
+        }
+    }
+}
