a4cb14db770011260eee7a316e8d032981bc291d
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-17 09:43:27 +0530

    Inject EntityManager using @PersistenceContext in Test servlet

--- src/main/java/lk/kaushalya/bcd/jta/Test.java
+++ src/main/java/lk/kaushalya/bcd/jta/Test.java
@@ -1,6 +1,8 @@
 package lk.kaushalya.bcd.jta;
 
+
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,6 +14,10 @@ import java.util.List;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
+
+    @PersistenceContext(unitName = "JTA-PU")
+    private EntityManager em;
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -20,7 +26,7 @@ public class Test extends HttpServlet {
 //        System.out.println(emf.hashCode());
 //        EntityManager em = emf.createEntityManager();
 
-        EntityManager em = ManagerFactory.getEntityManager();
+ //       EntityManager em = ManagerFactory.getEntityManager();
 
         List<Object> result = em.createNativeQuery("SELECT * FROM user").getResultList();
         req.setAttribute("yourDataAttribute", result);
