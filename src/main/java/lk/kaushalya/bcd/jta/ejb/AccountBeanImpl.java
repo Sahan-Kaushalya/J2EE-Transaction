5db895df8feb1dbac9790d5a4d823bc33affe37a
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-18 11:34:54 +0530

    Implement creditAmount and debitAmount methods with error handling in AccountBeanImpl

--- src/main/java/lk/kaushalya/bcd/jta/ejb/AccountBeanImpl.java
+++ src/main/java/lk/kaushalya/bcd/jta/ejb/AccountBeanImpl.java
@@ -1,16 +1,47 @@
 package lk.kaushalya.bcd.jta.ejb;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import lk.kaushalya.bcd.jta.entity.Account;
+
 import java.math.BigDecimal;
 
 public class AccountBeanImpl implements AccountBean{
+
+    @PersistenceContext(unitName = "JTA-PU")
+    private EntityManager em;
+
     @Override
     public void creditAmount(Long accountNo, BigDecimal amount) {
+
+        try {
+            Account account =  em.createNamedQuery("Account.findByAccountNo", Account.class)
+                      .setParameter("accountNo", accountNo)
+                      .getSingleResult();
+
+            account.setBalance(account.getBalance().add(amount));
+            em.merge(account);
+
+        } catch (NoResultException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void debitAmount(Long accountNo, BigDecimal amount) {
+        try {
+            Account account =  em.createNamedQuery("Account.findByAccountNo", Account.class)
+                    .setParameter("accountNo", accountNo)
+                    .getSingleResult();
 
+            account.setBalance(account.getBalance().subtract(amount));
+            em.merge(account);
+
+        } catch (NoResultException e) {
+            e.printStackTrace();
+        }
     }
 }
 
