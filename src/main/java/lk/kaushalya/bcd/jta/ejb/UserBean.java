41205f9f8860bb0359787dbb4eb925a7370ce344
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-18 12:12:08 +0530

    Add transfer method to UserBean interface for fund transfers

--- src/main/java/lk/kaushalya/bcd/jta/ejb/UserBean.java
+++ src/main/java/lk/kaushalya/bcd/jta/ejb/UserBean.java
@@ -2,8 +2,11 @@ package lk.kaushalya.bcd.jta.ejb;
 
 import jakarta.ejb.Local;
 
+import java.math.BigDecimal;
+
 @Local
 public interface UserBean {
     boolean login(String email, String password);
     boolean register(String name, String email, String address, String password);
+    void transfer(Long fromAccountNo, Long toAccountNo, BigDecimal amount);
 }
