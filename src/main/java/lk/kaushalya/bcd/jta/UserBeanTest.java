bd348f1ee82141d5b21bfa3ae11ec40c3ae81ebc
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-18 10:27:17 +0530

    Enhance UserBeanTest servlet to support dynamic user registration with random addresses

--- src/main/java/lk/kaushalya/bcd/jta/UserBeanTest.java
+++ src/main/java/lk/kaushalya/bcd/jta/UserBeanTest.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lk.kaushalya.bcd.jta.ejb.UserBean;
 
 import java.io.IOException;
+import java.util.Random;
 
 @WebServlet("/test-register")
 public class UserBeanTest extends HttpServlet {
@@ -16,14 +17,25 @@ public class UserBeanTest extends HttpServlet {
     private UserBean userBean;
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
 
-        boolean result = userBean.register("Sahan","sahan@gmail.com","Kirindiwela","S1234");
+        if (username != null) {
 
-        if(result){
-            resp.getWriter().write("Registration successful");
-        }else {
-            resp.getWriter().write("Registration failed");
+            String[] addresses = {"Kirindiwela", "Colombo", "Kandy", "Gampaha", "Galle", "Kurunegala"};
+            Random random = new Random();
+            int randomIndex = random.nextInt(addresses.length);
+            String randomAddress = addresses[randomIndex];
+
+            boolean result = userBean.register(username,username.toLowerCase()+"@gmail.com",randomAddress,username.toUpperCase().charAt(0)+"1234");
+
+            if(result){
+                resp.getWriter().write("Registration successful");
+            }else {
+                resp.getWriter().write("Registration failed");
+            }
+        }else{
+            resp.getWriter().write("Username is required");
         }
     }
 }
