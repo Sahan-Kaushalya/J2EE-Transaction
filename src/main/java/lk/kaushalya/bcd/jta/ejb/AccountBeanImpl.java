412833dd689bd2eac22bfb9559369402f1f1e1db
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-18 11:11:58 +0530

    Implement AccountBeanImpl with creditAmount and debitAmount methods

---
+++ src/main/java/lk/kaushalya/bcd/jta/ejb/AccountBeanImpl.java
@@ -0,0 +1,16 @@
+package lk.kaushalya.bcd.jta.ejb;
+
+import java.math.BigDecimal;
+
+public class AccountBeanImpl implements AccountBean{
+    @Override
+    public void creditAmount(Long accountNo, BigDecimal amount) {
+
+    }
+
+    @Override
+    public void debitAmount(Long accountNo, BigDecimal amount) {
+
+    }
+}
+
